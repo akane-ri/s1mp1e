8e04514b8d80a315926c6e3a25350d92f6c0e928
OliaZavyalova <138235021+akane-ri@users.noreply.github.com>
2024-03-14 21:59:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function inputVisible() {
+    document.getElementById('inputSearch').style.display = 'block';
+    document.getElementById('inputSearch').focus();
+    document.getElementById('exitInput').style.display = 'block';
+}
+function inputInvisible() {
+    document.getElementById('inputSearch').style.display = 'none';
+    document.getElementById('exitInput').style.display = 'none';
+}
+function downMenuVisible() {
+    document.getElementById('downMenu').style.display = 'block';
+}
+function downMenuInVisible() {
+    document.getElementById('downMenu').style.display = 'none';
+}
+
